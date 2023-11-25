b988af8f6071f35701395539dabc42c781e5ee52
赵猛 <zhaomeng@daojia-inc.com>
2023-11-25 21:30:37 +0800

    deep admin

---
+++ src/BackupServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Andruby\Data\Backup;
+
+use Andruby\Pay\Console\BackupCommand;
+use Illuminate\Support\ServiceProvider;
+
+class BackupServiceProvider extends ServiceProvider
+{
+
+    protected $routeMiddleware = [
+
+    ];
+
+    protected $commands = [
+        BackupCommand::class
+    ];
+
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+            $this->publishes([
+                __DIR__ . '/../config/data_backup.php' => config_path('data_backup.php'),
+            ]);
+        }
+
+        $this->loadRoutesFrom(__DIR__ . '/../routes/route.php');
+
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/data_backup.php', 'data_backup');
+
+        $this->registerRouteMiddleware();
+
+        $this->commands($this->commands);
+    }
+
+
+    protected function registerRouteMiddleware()
+    {
+        // register route middleware.
+        foreach ($this->routeMiddleware as $key => $middleware) {
+            app('router')->aliasMiddleware($key, $middleware);
+        }
+
+    }
+}
